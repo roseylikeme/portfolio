630b5728c39d6397d4d22f8660107a8b358fdd9b
Roselene Gabun <roselenegabun@gmail.com>
2024-03-11 22:42:10 -0700

    debounce

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -1,38 +1,57 @@
-/*!
-=========================================================
-* Meyawo Landing page
-=========================================================
 
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
 
 // smooth scroll
-$(document).ready(function(){
-    $(".navbar .nav-link").on('click', function(event) {
+$(document).ready(function () {
+    // Function to toggle navbar based on window size
+    var toggleNavbar = function () {
+        if ($(window).width() > 768) {
+            $('#nav-toggle').addClass('is-active');
+            $('ul.nav').addClass('show');
+        } else {
+            $('#nav-toggle').removeClass('is-active');
+            $('ul.nav').removeClass('show');
+        }
+    };
 
+    // Call toggleNavbar initially
+    toggleNavbar();
+
+    // Debounced function for toggling navbar on window resize
+    var debouncedToggleNavbar = debounce(toggleNavbar, 250);
+
+    // Bind debounced function to window resize event
+    $(window).on('resize', debouncedToggleNavbar);
+
+    // Smooth scrolling for navbar links
+    $(".navbar .nav-link").on('click', function (event) {
         if (this.hash !== "") {
-
             event.preventDefault();
-
             var hash = this.hash;
-
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 700, function(){
+            }, 700, function () {
                 window.location.hash = hash;
             });
-        } 
+        }
     });
-});
 
-// navbar toggle
-$('#nav-toggle').click(function(){
-    $(this).toggleClass('is-active')
-    $('ul.nav').toggleClass('show');
+    // Navbar toggle
+    $('#nav-toggle').click(function () {
+        $(this).toggleClass('is-active');
+        $('ul.nav').toggleClass('show');
+    });
 });
